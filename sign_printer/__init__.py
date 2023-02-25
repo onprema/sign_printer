98d0014dfe4bd25a13c1c354827bcbc2c66d610b
Lee Gaines <lgaines@autodesk.com>
2023-02-25 17:43:27 -0500

    add utils

---
+++ sign_printer/__init__.py
@@ -0,0 +1,11 @@
+"""
+sign_printer
+
+This is a simple Python app that prints ASCII art text entered by the user.
+
+Usage: python sign_printer.py <text>
+
+Contents:
+- sign_printer.py: The core application
+- utils.py: Utility functions and variables
+"""
