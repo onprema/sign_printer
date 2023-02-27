2cd596243a0bcce9dc0442aeee168435724043d3
Lee Gaines <lee.gaines@autodesk.com>
2023-02-27 14:49:08 -0500

    fix newline

--- sign_printer/sign_printer.py
+++ sign_printer/sign_printer.py
@@ -10,11 +10,11 @@ from pyfiglet import Figlet
 def main():
     """Main function gets called when invoked from the command-line"""
     figlet = Figlet(font=utils.random_font())
-    text_to_print = ' '.join(sys.argv[1:])
+    text_to_print = " ".join(sys.argv[1:])
     print(figlet.renderText(text_to_print))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not utils.validate_args(sys.argv):
         print(utils.USAGE)
         sys.exit(1)
